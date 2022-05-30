5f44797a2f11200a86dc28f7db526b173b6830e9
suveiko <suveikosasha@gmail.com>
2022-05-30 14:56:09 +0400

    Add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {BoardCount} from "./components/boardcount/BoardCount";
 import {SettingsCounter} from "./SettingsCounter";
@@ -7,26 +7,51 @@ function App() {
     const [minValue, setMinValue] = useState(0)
     const [maxValue, setMaxValue] = useState(5)
     const [counter, setCounter] = useState(minValue)
+    const [status, setStatus] = useState('counter')
 
 
-    const incButton = () => {
-        if (maxValue === counter) return
-        setCounter(counter + 1)
-    }
+    // const incButton = () => {
+    //     if (maxValue === counter) return
+    //     setCounter(counter + 1)
+    // }
+    // const resetButton = () => setCounter(minValue)
+    //
+    // const changeMaxValue = () => {
+    //     setMaxValue(maxValue + 1)
+    // }
+    //
+    // const changeMinValue = () => {
+    //     if (minValue === maxValue) return
+    //     setMinValue(minValue + 1)
+    // }
+    useEffect(() => {
+        let startValueAsString = localStorage.getItem('startValue')
+        if (startValueAsString) {
+            let newStartValue = JSON.parse(startValueAsString)
+            setMinValue(newStartValue)
+        }
+        let maxValueAsString = localStorage.getItem('maxValue')
+        if (maxValueAsString) {
+            let newMaxValue = JSON.parse(maxValueAsString)
+            setMaxValue(newMaxValue)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('startValue', JSON.stringify(minValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    }, [minValue, maxValue])
+
+
+    const incButton = () => counter < maxValue && setCounter(counter + 1)
     const resetButton = () => setCounter(minValue)
 
-    const changeMaxValue = () => {
-        setMaxValue(maxValue + 1)
-    }
-
-    const changeMinValue = () => {
-        if (minValue === maxValue) return
-        setMinValue(minValue + 1)
-    }
+    const changeMinValue = (value: number) => setMinValue(value)
+    const changeMaxValue = (value: number) => setMaxValue(value)
 
     return (
         <div className='App'>
-            <div className="btn">
+            <div className="desk">
                 <BoardCount
                     counter={counter}
                     callBackInc={incButton}
@@ -35,8 +60,9 @@ function App() {
                     maxValue={maxValue}
                 />
             </div>
-            <div className="btn">
+            <div className="desk">
                 <SettingsCounter
+                    counter={counter}
                     maxValue={maxValue}
                     minValue={minValue}
                     changeMaxValue={changeMaxValue}
--- src/SettingsCounter.tsx
+++ src/SettingsCounter.tsx
@@ -5,8 +5,9 @@ import s from './components/buttons/Buttons.module.css'
 type SettingsCounter = {
     maxValue: number
     minValue: number
-    changeMaxValue: () => void
-    changeMinValue: () => void
+    changeMaxValue: (value: number) => void
+    changeMinValue: (value: number) => void
+    counter: number
 }
 
 export const SettingsCounter = ({minValue, maxValue, changeMaxValue, changeMinValue}: SettingsCounter) => {
@@ -14,11 +15,11 @@ export const SettingsCounter = ({minValue, maxValue, changeMaxValue, changeMinVa
         <div>
             <div>
                 <span style={{color:'white'}}>Min Value:</span>
-                <input type="number" value={minValue} onChange={changeMinValue}/>
+                <input type="number" value={minValue} onChange={(e) => changeMinValue(+e.currentTarget.value)}/>
             </div>
             <div>
                 <span style={{color:'white'}}>Max Value:</span>
-                <input type="number" value={maxValue} onChange={changeMaxValue}/>
+                <input type="number" value={maxValue} onChange={(e)=>changeMaxValue(+e.currentTarget.value)}/>
             </div>
 
             <UniversalButton className={s.button} onClick={()=>{}} disabled={false} name='set'/>
